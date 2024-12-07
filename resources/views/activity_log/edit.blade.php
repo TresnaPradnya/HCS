afb9bf0c297c224614d1a8af5a6bc51a834135b1
Far-021 <64468825+Far-021@users.noreply.github.com>
2024-12-07 20:15:03 +0800

    Update edit.blade.php
    add input fields for the values

--- resources/views/activity_log/edit.blade.php
+++ resources/views/activity_log/edit.blade.php
@@ -69,6 +69,15 @@
                                     <span id="commuting_method_id" class="error invalid-feedback">{{ $message }}</span>
                                 @enderror
                             </div>
+                            <div class="form-group">
+                                <label for="commuting_method_value">Commuting Method Value</label>
+                                <input type="number" class="form-control @error('commuting_method_value') is-invalid @enderror"
+                                    id="commuting_method_value" name="commuting_method_value"
+                                    value="{{ old('commuting_method_value', $al->commuting_method_value) }}">
+                                @error('commuting_method_value')
+                                    <span id="commuting_method_value" class="error invalid-feedback">{{ $message }}</span>
+                                @enderror
+                            </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
@@ -86,6 +95,15 @@
                                 </select>
                                 @error('energy_source_id')
                                     <span id="energy_source_id" class="error invalid-feedback">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="energy_source_value">Energy Source Value</label>
+                                <input type="number" class="form-control @error('energy_source_value') is-invalid @enderror"
+                                    id="energy_source_value" name="energy_source_value"
+                                    value="{{ old('energy_source_value', $al->energy_source_value) }}">
+                                @error('energy_source_value')
+                                    <span id="energy_source_value" class="error invalid-feedback">{{ $message }}</span>
                                 @enderror
                             </div>
                         </div>
