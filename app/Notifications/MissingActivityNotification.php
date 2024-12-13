c700abfe5da7abcf3dc27e55a0f9b7c6b7dc88b5
Tresna Pradnya <dewipradnya999@gmail.com>
2024-12-13 22:29:26 +0800

    update code for reminder notification

--- app/Notifications/MissingActivityNotification.php
+++ app/Notifications/MissingActivityNotification.php
@@ -35,10 +35,10 @@ class MissingActivityNotification extends Notification
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-        ->subject('Pengingat Aktivitas Harian')
-        ->line('Kami melihat Anda belum membuat aktivitas untuk hari ini.')
-        ->action('Tambahkan Aktivitas', url('activity-log/index'))
-        ->line('Pastikan Anda mencatat aktivitas Anda setiap hari!');
+        ->subject('HCS - Activity Log Reminder')
+        ->line('We noticed you have not created an activity for today.')
+        ->action('Add Activity', url('activity-log/index'))
+        ->line('Make sure you record your activities every day.!');
     }
 
     /**
