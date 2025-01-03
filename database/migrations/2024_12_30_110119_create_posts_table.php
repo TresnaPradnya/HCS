437784b19d881b74b7b866322dcbec366d8bbfc0
Far-021 <64468825+Far-021@users.noreply.github.com>
2025-01-03 13:54:15 +0800

    add table for posts, interactions, and notifications

---
+++ database/migrations/2024_12_30_110119_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->text('content')->nullable();
+            $table->float('transportation_footprint')->nullable();
+            $table->float('energy_footprint')->nullable();
+            $table->float('diet_footprint')->nullable();
+            $table->float('total_footprint')->nullable();
+            $table->longText('pie_chart_image')->nullable();
+            $table->longText('line_chart_image')->nullable();
+            $table->timestamps();
+
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
